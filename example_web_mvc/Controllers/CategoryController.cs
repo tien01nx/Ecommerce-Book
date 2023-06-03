adab2ec988073a43232c6a41725ba16ac6fa8549
Nguyễn Văn Tiến <114860203+tien01nx@users.noreply.github.com>
2023-06-03 14:41:36 +0700

    api

--- example_web_mvc/Controllers/CategoryController.cs
+++ example_web_mvc/Controllers/CategoryController.cs
@@ -4,134 +4,77 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace example_web_mvc.Controllers
 {
-
-    public class CategoryController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
     {
-
         private readonly ApplicationDbContext _db;
+
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
         }
-        public IActionResult Index()
+
+        // GET: api/Category
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetCategories()
         {
-            List<Category> obj = _db.Categories.ToList();
-            return View(obj);
+            return _db.Categories.ToList();
         }
 
-        public IActionResult Create()
+        // GET: api/Category/5
+        [HttpGet("{id}")]
+        public ActionResult<Category> GetCategory(int id)
         {
+            var category = _db.Categories.Find(id);
 
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(Category obj)
-        {
-
-            // kiếm tra dữ liệu khi thêm
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "displayOrder cannot exactly match the Name");
-            //}
-            if (ModelState.IsValid)
-            {
-                // thêm dữ liệu vào database
-                _db.Categories.Add(obj);
-
-                // lưu dữ liệu khi thêm
-                _db.SaveChanges();
-                TempData["success"] = "Category created successfully";
-                return RedirectToAction("Index");
-            }
-
-            return View();
-
-
-
-        }
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-
-            }
-
-
-            Category? category = _db.Categories.Find(id);
-            //Category? category1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            //Category? category2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
             if (category == null)
             {
                 return NotFound();
             }
 
-            return View(category);
+            return category;
         }
 
-
-        [HttpPost]
-        public IActionResult Edit(Category obj)
+        // PUT: api/Category/5
+        [HttpPut("{id}")]
+        public IActionResult PutCategory(int id, Category category)
         {
-
-
-            if (ModelState.IsValid)
+            if (id != category.Id)
             {
-                // thêm dữ liệu vào database
-                _db.Categories.Update(obj);
-
-                // lưu dữ liệu khi thêm
-                _db.SaveChanges();
-                TempData["success"] = "Category update successfully";
-                return RedirectToAction("Index");
+                return BadRequest();
             }
 
-            return View();
-
-        }
-
-
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-
-            }
-
-
-            Category? category = _db.Categories.Find(id);
-            //Category? category1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            //Category? category2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePost(int? id)
-        {
-            Category? category = _db.Categories.Find(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            _db.Categories.Remove(category);
-
+            _db.Categories.Update(category);
             _db.SaveChanges();
-            TempData["success"] = "Delete category successfully";
 
-            return RedirectToAction("Index");
-
+            return NoContent();
         }
 
+        // POST: api/Category
+        [HttpPost]
+        public ActionResult<Category> PostCategory(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
 
+            return CreatedAtAction("GetCategory", new { id = category.Id }, category);
+        }
 
+        // DELETE: api/Category/5
+        [HttpDelete("{id}")]
+        public ActionResult<Category> DeleteCategory(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
 
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+
+            return category;
+        }
     }
 }
--- example_web_mvc/Program.cs
+++ example_web_mvc/Program.cs
@@ -1,4 +1,51 @@
-﻿using example_web_mvc.Data;
+﻿//using example_web_mvc.Data;
+//using Microsoft.EntityFrameworkCore;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//// Add CORS services
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("VueCorsPolicy", builder =>
+//    {
+//        builder
+//            .WithOrigins("https://localhost:8080") // Replace with the origin of your Vue app
+//            .AllowAnyHeader()
+//            .AllowAnyMethod()
+//            .AllowCredentials();
+//    });
+//});
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // Giá trị HSTS mặc định là 30 ngày. Bạn có thể muốn thay đổi điều này cho các kịch bản sản xuất, xem https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.Run();
+using example_web_mvc.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,13 +55,26 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("VueCorsPolicy", builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:8080") // Replace with the origin of your Vue app
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // Giá trị HSTS mặc định là 30 ngày. Bạn có thể muốn thay đổi điều này cho các kịch bản sản xuất, xem https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,8 +85,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Use CORS policy
+app.UseCors("VueCorsPolicy");
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Category}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
